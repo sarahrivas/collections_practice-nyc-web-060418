3ad8d48b0f2dc2ad9bda1d501316cc8b8a9a2dc0
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-05 11:37:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(int_array)
+  int_array.sort
+end
+
+def sort_array_desc(int_array)
+  int_array.sort do |a, b| b <=> a
+  end  
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort do |a, b| a.length <=> b.length
+  end 
+end
+
+def swap_elements(array)
+  #array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(int_array)
+  
+end
+
+def kesha_maker(string_array)
+  
+end
+
+def find_a(int_array)
+  
+end
+
+
+def sum_array(int_array)
+  
+end
+
+def add_s(array)
+  
+end
 
